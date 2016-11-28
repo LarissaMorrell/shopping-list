d86b0c7b91056429cc28e90b69bf460f950186b1
LarissaMorrell <larissa@larissamorrell.com>
2016-11-27 19:32:42 -0500

    delete and checked now working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 var state = {
     items: []
@@ -9,19 +9,28 @@ var addItem = function(state, item) {
     state.items.push(item);
 }
 
+var checkItem = function(item) {
+
+    console.log(item);
+    item.toggleClass("shopping-item__checked");
+}
+
 var deleteItem = function(state, item) {
 
-	//for loop to find state.items[i]
-
+    //for loop to find state.items[i]
+    for (var i = 0; i < state.items.length; i++) {
+        //once item === state.items[i]
+        if(item.text() === state.items[i]){
+        	state.items.splice(i, 1);
+        	break;
+        }
+    }
 }
 
 
 //Render functions
 var renderItem = function(state, element) {
     var itemsHTML = state.items.map(function(item) {
-
-        //var elemStr = '<li> <span class="shopping-item">' + item + '</span>';
-        //elemStr += '<div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
 
         return '<li> <span class="shopping-item">' + item + '</span>' +
             '<div class="shopping-item-controls">' +
@@ -41,30 +50,28 @@ $(document).ready(function() {
 
 
     //Event listeners
-    //$('.js-shopping-list-form').submit( function(event) {
-    $('button').click(function(event) {
-    //$('.js-shopping-list-form').on('click', 'button', function(event) {
+    $('#js-shopping-list-form').on('click', 'button', function(event) {
 
-        event.preventDefault();    //but what default action are we preventing?
+        event.preventDefault(); 
         addItem(state, $('#shopping-list-entry').val());
         renderItem(state, $('.js-shopping-list'));
     });
 
 
+    //check off the items
+    $(document).on('click', '.shopping-item-toggle', function(event) {
 
-    $('.shopping-item-toggle').click(function(event) {
-    	event.preventDefault();  //needed?
-
+        event.preventDefault(); 
+        checkItem($(this).closest('li').find('.shopping-item'));
 
     });
 
+    //delete and item
+    $(document).on('click', '.shopping-item-delete', function(event){
+        event.preventDefault(); 
 
-
-    $('shopping-item-delete').click(function(event) {
-    	event.preventDefault();  //needed?
-
-    	deleteItem(state, $(this)); 
-
-
+        //this button find closest parent li and then the shopping-item child
+        deleteItem(state, $(this).closest('li').find('.shopping-item'));
+        renderItem(state, $('.js-shopping-list'));
     });
 });
